af774a48540d3bc97e6f5bdd7adf130c4f1e8d52
Abod Abbas <abdelrahman.abbas@ucalgary.ca>
2024-10-15 10:58:35 -0600

    added basic testing

--- src/test/scala/SPITest.scala
+++ src/test/scala/SPITest.scala
@@ -1,4 +1,4 @@
-/*
+
 package tech.rocksavage.chiselware.SPI
 
 import org.scalatest.Assertions._
@@ -22,107 +22,29 @@ class SPIModuleTest extends AnyFlatSpec with ChiselScalatestTester {
   val myParams =
     BaseParams(true, 1, 1000, 8, 8)
 
-  // Helper function to run the master-slave transmission
-  def runMasterTransmission(dut: SPIMaster): Unit = {
-    // Write data from APB to SPI
-    dut.io.apb.PWDATA.poke(0xa5.U)
-    dut.io.apb.PSEL.poke(true.B)
-    dut.io.apb.PWRITE.poke(true.B)
-    dut.io.apb.PENABLE.poke(true.B)
-    // Check if data is being shifted out correctly on MOSI
-    for (i <- 7 until -1) {
-      dut.clock.step(1)
-      println(s"MOSI: ${dut.io.pins.mosi.peek().litValue}")
-      dut.io.pins.mosi.expect(0xa5.U & (1 << i).U)
-    }
+  def toggleSCLK(cycles: Int = spiClockPeriod / 2): Unit = {
+    poke(dut.io.sclk, true.B)
+    step(cycles)
+    poke(dut.io.sclk, false.B)
+    step(cycles)
   }
 
-  // Tests for Mode 0
-  it should "work as an SPI master in Mode0" in {
-    test(new SPIMaster(myParams))
-      .withAnnotations(backendAnnotations) { dut =>
-        test(new SPI(myParams, 1000000, SPIMode.Mode0, SPIRole.Master))
-      }
+  // Enable SPI
+  poke(dut.io.apb.PSEL, true.B)
+  poke(dut.io.apb.PADDR, 0x00.U)  // CTRLA address
+  poke(dut.io.apb.PWRITE, true.B)
+  poke(dut.io.apb.PWDATA, 0x01.U) // Enable SPI
+  poke(dut.io.apb.PENABLE, true.B)
+  step(1)
+
+  // Simulate data transfer
+  poke(dut.io.mosi, 0xAA.U)  // Send 0xAA on MOSI
+  for (i <- 0 until 8) {
+    toggleSCLK()  // Toggle SCLK for each bit
   }
 
-  it should "work as an SPI slave in Mode0" in {
-    test(new SPI(myParams, 1000000, SPIMode.Mode0, SPIRole.Slave))
-      .withAnnotations(backendAnnotations) { dut =>
-        for (i <- 0 until 8) {
-          dut.io.spi.mosi.poke((i % 2 == 0).B)
-          dut.io.spi.sclk.poke(false.B)
-          dut.clock.step(1)
-          dut.io.spi.sclk.poke(true.B)
-          dut.clock.step(1)
-        }
-        dut.io.apb.PRDATA.expect(0xaa.U)
-      }
-  }
-
-  // Tests for Mode 1
-  it should "work as an SPI master in Mode1" in {
-    test(new SPI(myParams, 1000000, SPIMode.Mode1, SPIRole.Master))
-      .withAnnotations(backendAnnotations) { dut =>
-        runMasterTransmission(dut, SPIMode.Mode1)
-      }
-  }
-
-  it should "work as an SPI slave in Mode1" in {
-    test(new SPI(myParams, 1000000, SPIMode.Mode1, SPIRole.Slave))
-      .withAnnotations(backendAnnotations) { dut =>
-        for (i <- 0 until 8) {
-          dut.io.spi.mosi.poke((i % 2 == 0).B)
-          dut.io.spi.sclk.poke(true.B)
-          dut.clock.step(1)
-          dut.io.spi.sclk.poke(false.B)
-          dut.clock.step(1)
-        }
-        dut.io.apb.PRDATA.expect(0xaa.U)
-      }
-  }
-
-  // Tests for Mode 2
-  it should "work as an SPI master in Mode2" in {
-    test(new SPI(myParams, 1000000, SPIMode.Mode2, SPIRole.Master))
-      .withAnnotations(backendAnnotations) { dut =>
-        runMasterTransmission(dut, SPIMode.Mode2)
-      }
-  }
-
-  it should "work as an SPI slave in Mode2" in {
-    test(new SPI(myParams, 1000000, SPIMode.Mode2, SPIRole.Slave))
-      .withAnnotations(backendAnnotations) { dut =>
-        for (i <- 0 until 8) {
-          dut.io.spi.mosi.poke((i % 2 == 0).B)
-          dut.io.spi.sclk.poke(false.B)
-          dut.clock.step(1)
-          dut.io.spi.sclk.poke(true.B)
-          dut.clock.step(1)
-        }
-        dut.io.apb.PRDATA.expect(0xaa.U)
-      }
-  }
-
-  // Tests for Mode 3
-  it should "work as an SPI master in Mode3" in {
-    test(new SPI(myParams, 1000000, SPIMode.Mode3, SPIRole.Master))
-      .withAnnotations(backendAnnotations) { dut =>
-        runMasterTransmission(dut, SPIMode.Mode3)
-      }
-  }
-
-  it should "work as an SPI slave in Mode3" in {
-    test(new SPI(myParams, 1000000, SPIMode.Mode3, SPIRole.Slave))
-      .withAnnotations(backendAnnotations) { dut =>
-        for (i <- 0 until 8) {
-          dut.io.spi.mosi.poke((i % 2 == 0).B)
-          dut.io.spi.sclk.poke(true.B)
-          dut.clock.step(1)
-          dut.io.spi.sclk.poke(false.B)
-          dut.clock.step(1)
-        }
-        dut.io.apb.PRDATA.expect(0xaa.U)
-      }
-  }
+  // Check if data was received correctly
+  expect(dut.io.miso, 0x55.U)  // Check if 0x55 was received on MISO
+  println("Data transfer test passed.")
 }
- */
+
